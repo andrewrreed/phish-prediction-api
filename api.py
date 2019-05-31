9ecd6ca052519dfeb09ed9ff2f02cb306b44c443
Andrew Reed <areed1242@gmail.com>
2019-05-31 12:59:24 -0400

    fix enpoint returns

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import os
 import util
 import numpy as np
 from keras.models import load_model
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 model = None
@@ -29,7 +29,7 @@ def home_endpoint():
 
 @app.route('/next-song', methods=['POST'])
 def predict_next_song():
-    # takes in a numpy array of length 150 representing encoded songs
+    '''Takes in a numpy array of length 150 representing encoded songs and returns a song prediction as string'''
     if request.method == 'POST':
         data = request.get_json()
         data_formatted = np.array([data]) # converts shape from (150,) --> (1,150)
@@ -40,7 +40,7 @@ def predict_next_song():
 
 @app.route('/generate-setlist', methods=['POST'])
 def generate_next_setlist():
-    # takes in a numpy array of length 150 representing encoded songs
+    '''Takes in a numpy array of length 150 representing encoded songs and returns a dictionary with the input and generated sequences in english readable text'''
     if request.method == 'POST':
         data = request.get_json()
         generated_sequence = util.generate_full_setlist(model, data)
@@ -48,8 +48,9 @@ def generate_next_setlist():
         generated_sequence_clean = [idx_to_song[idx] for idx in generated_sequence]
         input_sequence_clean = [idx_to_song[idx] for idx in data]
 
-    return generated_sequence_clean
+        response = {'input_sequence':input_sequence_clean, 'generated_sequence':generated_sequence_clean}
 
+    return jsonify(response)
 
 
 #------------------ Run Application ------------------ 
